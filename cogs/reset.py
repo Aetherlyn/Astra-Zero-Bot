2a299b3fb977f359f459802889ce607bcd71567c
Aetherlyn <ahmetaydincapar@gmail.com>
2026-04-21 11:25:26 +0300

    feat(reset): add !reset armor subcommand to reset armor proficiencies

--- cogs/reset.py
+++ cogs/reset.py
@@ -236,6 +236,21 @@ class Reset(commands.Cog):
         else:
             await ctx.send("Cancelled.")
 
+    @reset.command()
+    async def armor(self, ctx):
+        view = ConfirmView(ctx.author)
+
+        await ctx.send("Do you really wish to reset your armor proficiencies?", view=view)
+
+        await view.wait()
+        
+        if view.value is None:
+            await ctx.send("Timed out.")
+        elif view.value:
+            write_character(ctx.guild.id, ctx.author.id, "armor", "")
+            await ctx.send("Armor proficiencies have been reset.")
+        else:
+            await ctx.send("Cancelled.")
 
 def setup(bot: commands.Bot):
     bot.add_cog(Reset(bot))
